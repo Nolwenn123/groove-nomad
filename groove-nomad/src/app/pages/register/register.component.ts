aa9999e4693374627d196d2b95dd1bded4453921
Nolwenn123 <nolwenn.bourey@gmail.com>
2025-07-16 13:37:06 +0200

    add register style

--- groove-nomad/src/app/pages/register/register.component.ts
+++ groove-nomad/src/app/pages/register/register.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
   currentStep = 1;
