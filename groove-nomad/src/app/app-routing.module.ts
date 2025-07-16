447f25a1b7a4ca6e04d2f1e143af0a2fafc46818
Nolwenn123 <nolwenn.bourey@gmail.com>
2025-07-16 23:29:28 +0200

    change some things

--- groove-nomad/src/app/app-routing.module.ts
+++ groove-nomad/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FestivalFormComponent } from './festival-form/festival-form.component';
 import { RegisterComponent } from './pages/register/register.component';
+import { ReservationComponent } from './pages/reservation/reservation.component';
 
 const routes: Routes = [
   {path:'form', component: FestivalFormComponent},
-  {path:'register', component: RegisterComponent}
+  {path:'register', component: RegisterComponent},
+  {path:'reservation', component: ReservationComponent}
 
 ];
 
